972b27aa7ddac4085eca7e4430aaa22470e2f7f3
Dmitry Dzhus <dima@sphinx.net.ru>
2010-03-06 19:31:39 +0300

    Tree interface now uses 0-based indexing.

--- lac.cpp
+++ lac.cpp
@@ -38,8 +38,11 @@ private:
     row in_times, out_times;
     small_int in_timer, out_timer;
 
-    /// 2^j-th ancestors of each node and distances to ancestors.
+    /// 2^j-th ancestors of each node.
     matrix anc;
+
+    /// Distances to 2^j-th ancestors of each node. We use long
+    /// integers here to fit maximum value of 40000×39999.
     lmatrix dist;
 
     /// Traverse into v with p as parent
@@ -66,8 +69,8 @@ private:
     small_int lac_proc(small_int v1, small_int v2)
     {
         for (int j = levels; j >= 0; j--)
-            if (!is_ancestor(anc[v1][j] + 1, v2))
-                v1 = anc[v1][j] + 1;
+            if (!is_ancestor(anc[v1][j], v2))
+                v1 = anc[v1][j];
         return anc[v1][0];
     }
 
@@ -106,10 +109,10 @@ public:
 
     }
 
-    /// Add edge from v1 to v2 with given length (1-based indexing)
+    /// Add edge from v1 to v2 with given length
     void add_edge(small_int v1, small_int v2, small_int length)
     {
-        m[v1 - 1][v2 - 1] = length;
+        m[v1][v2] = length;
     }
 
     void lac_preprocess(void)
@@ -121,19 +124,20 @@ public:
     /// Return true if v1 is ancestor of v2 (1-based indexing)
     bool is_ancestor(small_int v1, small_int v2)
     {
-        cout << v1 << " vs " << v2 << endl;
-                        
-        return (in_times[v1 - 1] < in_times[v2 - 1]) && 
-            (out_times[v1 - 1] > out_times[v2 - 1]);
+        return (in_times[v1] < in_times[v2]) && 
+            (out_times[v1] > out_times[v2]);
     }
 
     small_int find_lac(small_int v1, small_int v2)
     {
+        small_int v;
         if (is_ancestor(v1, v2))
-            return v1;
+            v = v1;
         else if (is_ancestor(v2, v1))
-            return v2;
-        else return lac_proc(v1, v2);
+            v = v2;
+        else 
+            v = lac_proc(v1, v2);
+        return v + 1;
     }
 };
 
@@ -148,17 +152,16 @@ int main(int argc, char* argv[])
     for (small_int i = 0; i < size - 1; i++)
     {
         cin >> a >> b >> length;
-        tree->add_edge(a, b, length);
+        tree->add_edge(a - 1, b - 1, length);
     }
     tree->lac_preprocess();
-    tree->print();
     
     cin >> pairs;
     
     for (small_int i = 0; i < pairs; i++)
     {
         cin >> a >> b;
-        cout << tree->find_lac(a, b) << endl;
+        cout << tree->find_lac(a - 1, b - 1) << endl;
     }
 
     delete tree;
